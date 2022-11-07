0a86915141746a5cb461b92d3802d4cb7d123cb1
Andy <nandy1990>
2022-11-07 20:19:10 +0100

    example for ali ready

--- main.py
+++ main.py
@@ -1,22 +1,14 @@
 
 import uvicorn
-
+from fastapi import FastAPI
 import os
-from app.EmailProvider import EmailProvider
-from app.provider.Provider_sendgrid import Provider_sendgrid
+from routers import example_router
 
 app = FastAPI()
-app.include_router(email.router)
+app.include_router(example_router.router)
 
-lsfastapi.Wrapper(app, init_mongoengine=False)
-eswebapi.initClients(webapispec=webapispec)
-providers = ["SENDGRID"]
 
-provider = os.environ.get("EMAIL_PROVIDER", "SENDGRID")
-if provider not in providers:
-	raise Exception("provider not found")
 
-EmailProvider.provider = Provider_sendgrid()
 
 if __name__ == "__main__":
 	uvicorn.run("main:app", host="0.0.0.0", port=9000, log_level="info", reload=True)
